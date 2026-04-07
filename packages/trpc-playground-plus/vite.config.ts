f53164ef45ac9f4b5c2d41c2949bee448e5d1d18
Remy Mulet <remy.mulet@gmail.com>
2026-04-07 22:54:03 +0200

    chore: add basic ci with lint, typecheck & build

--- packages/trpc-playground-plus/vite.config.ts
+++ packages/trpc-playground-plus/vite.config.ts
@@ -17,7 +17,16 @@ export default defineConfig({
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', '@trpc/client', '@trpc/server', 'node:path', 'node:fs', 'node:url', '@fastify/static'],
+      external: [
+        'react',
+        'react-dom',
+        '@trpc/client',
+        '@trpc/server',
+        'node:path',
+        'node:fs',
+        'node:url',
+        '@fastify/static',
+      ],
     },
     outDir: 'dist',
     emptyOutDir: true,
